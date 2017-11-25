2cb51eb567228b18a920e2a8e78414e99d7190d0
Mohammed Jasam <mnqnd@mst.edu>
2017-11-25 00:11:46 -0600

    Working on yolo

--- Past/Main/Model/script.py
+++ Past/Main/Model/script.py
@@ -18,6 +18,6 @@ tensorboard --logdir tf_files/training_summaries &
 python35 -m scripts.retrain -h
 
 # ReTrain the model using new training images!
-python35 -m scripts.retrain --bottleneck_dir=tf_files/bottlenecks --how_many_training_steps=4000 --model_dir=tf_files/models/ --summaries_dir=tf_files/training_summaries/"mobilenet_0.50_224" --output_graph=tf_files/retrained_graph.pb --output_labels=tf_files/retrained_labels.txt --architecture="mobilenet_0.50_224" --image_dir=tf_files/trainData
+python35 -m scripts.retrain --bottleneck_dir=tf_files/bottlenecks --how_many_training_steps=500 --model_dir=tf_files/models/ --summaries_dir=tf_files/training_summaries/"mobilenet_0.50_224" --output_graph=tf_files/retrained_graph.pb --output_labels=tf_files/retrained_labels.txt --architecture="mobilenet_0.50_224" --image_dir=tf_files/trainData
 
 python35 -m scripts.label_image --graph=tf_files/retrained_graph.pb --image=C:\Users\Stark\Desktop\CleverNator\
